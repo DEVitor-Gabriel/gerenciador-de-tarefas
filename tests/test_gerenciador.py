5531b84f3bddae205e509cdd174cb495c6aeb570
Vitor Gabriel <vitorg.34825@gmail.com>
2022-05-20 10:59:15 -0300

    teste3

--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,7 +1,7 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from gerenciador_tarefas.gerenciador import TAREFAS, app
+from gerenciador_tarefas.gerenciador import app
 
 
 def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200():
